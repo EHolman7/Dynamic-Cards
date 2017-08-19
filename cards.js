c7cd99dd12b101d6f0221830632a9dbdd20e2e58
holmanhub <holman.eric85@gmail.com>
2017-08-19 11:49:58 -0500

    finished function of create and delete

--- cards.js
+++ cards.js
@@ -1,54 +1,38 @@
-console.log("linked to cards.js")
+console.log("linked to cards.js");
 
-var inputField = document.getElementById('createText');
-var getCreateButton = document.getElementById('createbutton');
-var txt = "";
-var cardArray = "";
+var inputField = document.getElementById('createText');//Text input
+var getCreateButton = document.getElementById('createButton');// create button
+var cardDiv = document.getElementById('cardDiv');//div to hold cards
 
-function add(card) {
-
-//Dynamically create and input type
-var element = document.createElement("input");
-}
-
-
-
-document.body.addEventListener('keypress', function (event){
-  //console.log("event", event.key);
-  if(event.key === "Enter"){
-    txt = inputField.value;
-
-    //domString
-    domString(txt);
-    //console.log("yay", results);
-  writeToDom(cardArray);
-}})
-
-
-
-
-
-
-
-function domString(cardz){
+//domstring for my cards
+function domString(){
 	var cardString = '';
-	for (var i=0; i<cardArray.length; i++){
-		var newCard = '';
-		newCard += `<div class="cardBox" id="cardBox-${i}">`;
-		newCard += `<div class="cardText"></div>`;
-		newCard += `<button id="deleteCard" value="delete">Delete</button>`;
-		newCard += `</div>`;
-		cardString += newCard;
-	}
-	console.log(cardString)
+		cardString += `<div class="cardBox" id="cardBox">`;
+		cardString += `<div class="cardText">${inputField.value}</div>`;
+		cardString += `<button class="dbtn" id="deleteCard" value="delete">Delete</button>`;
+		cardString += `</div>`;
+	//console.log(cardString);
 	writeToDom(cardString);
+}
+//Function for my write to dom to send to html
+function writeToDom(cardString){
+	cardDiv.innerHTML += cardString;
+}
+// Create button event listener to run domsting
+getCreateButton.addEventListener('click', domString);
 
+// //have to put event listner on the cardholder since the delete button doesn't exist at first on the page. 
+cardDiv.addEventListener('click', removeCard);
 
+// //delete button function
+function removeCard (e){
+console.log(e);
+  if(e.target.className === 'dbtn'){
+    console.log("yaaaaaaaaa!");
+    e.target.parentNode.remove();
+  }
 
-
-
-
-
+}
 
 
 
